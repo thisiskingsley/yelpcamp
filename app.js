805763a14f47c94482b4a8d812e2f7fe610958c7
john doe <john@doee.org>
2020-07-31 04:12:43 +0000

    update db url

--- app.js
+++ app.js
@@ -20,12 +20,13 @@ mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
-// mongoose.connect("mongodb://localhost:27017/yelp_camp_v12");
-mongoose.connect("mongodb+srv://sirkings:monking89@cluster0.w8xc8.mongodb.net/yelpcamp?retryWrites=true&w=majority").then(function(){
-	console.log("Connected to DB!");
-}).catch(function(err){
-	console.log("ERROR:", err.message);
-});
+
+mongoose.connect(process.env.DATABASEURL);
+// mongoose.connect("mongodb+srv://sirkings:monking89@cluster0.w8xc8.mongodb.net/yelpcamp?retryWrites=true&w=majority").then(function(){
+// 	console.log("Connected to DB!");
+// }).catch(function(err){
+// 	console.log("ERROR:", err.message);
+// });
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
